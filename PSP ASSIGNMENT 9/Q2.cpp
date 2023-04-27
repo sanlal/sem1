2f1089c68f41f435195c773004df6c9d93a05039
sanlal <98936087+sanlal@users.noreply.github.com>
2023-04-27 22:02:53 +0530

    Add files via upload

---
+++ PSP ASSIGNMENT 9/Q2.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+class pointType
+{
+protected:
+	int x, y;
+public:
+	pointType()
+	{
+		x = 0;
+		y = 0;
+	}
+	
+	pointType(int x, int y)
+	{
+		this->x = x;
+		this->y = y;
+	}
+	
+
+	int distanceFromXAxis(){ return y; }
+	int distanceFromYAxis(){ return x; }
+	int getX(){ return x; }
+	int getY(){ return y; }
+	
+	void set(int _x, int _y)
+	{
+		x = _x;
+		y = _y;
+	}
+	
+	void display()
+	{
+		cout << "(" << x << ", " << y << ")" << endl;
+	}
+	
+	double distanceFromOrigin()
+	{
+		return sqrt(x*x + y*y);
+	}
+	
+};
+
+class circleType : public pointType
+{
+protected:
+	int radius;
+public:
+	circleType() : pointType()
+	{
+		radius = 0;
+	}
+	
+	circleType(int _r, int _x, int _y) : pointType(_x, _y)
+	{
+		radius = _r;
+	}
+	
+	double getArea()
+	{
+		return M_PI * radius * radius;
+	}
+	
+	double getCircumference()
+	{
+		return 2 * M_PI * radius;
+	}
+	
+	void display()
+	{
+		cout << "Circle radius is : " << radius << endl;
+		cout << "Circle centre is : ";
+		pointType::display();
+	}
+	
+	void setCenter(int _x, int _y)
+	{
+		set(_x, _y);
+	}
+	
+	void setRadius(int _r)
+	{
+		radius = _r;
+	}
+	
+	int getRadius()
+	{
+		return radius;
+	}
+	
+	int getCentreX(){ return x; }
+	int getCentreY(){ return y; }
+};
+
+int main()
+{
+	circleType c(10, 5, 5);
+	c.display();
+	cout << "Area " << c.getArea() << endl;
+	cout << "Circumference: " << c.getCircumference() << endl;
+	return 0;
+}
